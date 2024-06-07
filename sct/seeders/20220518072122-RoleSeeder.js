78d8ef76d9c24456d9987d81377b9d51ffd9011e
kietanh286 <108953076+kietanh286@users.noreply.github.com>
2024-06-07 16:24:52 +0700

    all
    all

--- sct/seeders/20220518072122-RoleSeeder.js
+++ sct/seeders/20220518072122-RoleSeeder.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -11,43 +11,43 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
-     await queryInterface.bulkInsert('Roles', [{
-        name: 'admin',
-        id:1,
-        description: 'admin',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: 'user',
-        id:2,
-        description: 'user',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: 'hr',
-        id:3,
-        description: 'HR',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: 'supplier',
-        id:4,
-        description: 'supplier manager Dashboard',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }], {});
+    await queryInterface.bulkInsert('Roles', [{
+      name: 'admin',
+      id: 1,
+      description: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'user',
+      id: 2,
+      description: 'user',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'accountant',
+      id: 3,
+      description: 'accountant',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'supplier',
+      id: 4,
+      description: 'supplier manager Dashboard',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-     await queryInterface.bulkDelete('Roles', null, {});
+    await queryInterface.bulkDelete('Roles', null, {});
   }
 };
